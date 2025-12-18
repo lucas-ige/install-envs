59197ea248ddfc29556476065f641b616dbb5e4f
Lucas Bastien <134368131+lucas-ige@users.noreply.github.com>
2025-12-18 14:21:14 +0100

    Start work on installing netcdf-c

---
+++ netcdf-c/install.bash
@@ -0,0 +1,15 @@
+#!/bin/bash
+#
+# Copyright (c) 2025-now Institut des Géosciences de l'Environnement (UMR 5001).
+#
+# License: BSD 3-Clause "New" or "Revised" License (BSD-3-Clause).
+#
+# This script installs the netcdf-c library.
+#
+# This script should be run from its directory.
+#
+
+source ../parameters.conf
+source parameters.conf
+
+echo "$url_repo"
