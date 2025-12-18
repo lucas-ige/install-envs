d91e58affbe8cc6f5fc160eee99883e9a50adb85
Lucas Bastien <134368131+lucas-ige@users.noreply.github.com>
2025-12-18 15:08:31 +0100

    Start work on installing the HDF5 library

---
+++ hdf5/install.bash
@@ -0,0 +1,43 @@
+#!/bin/bash
+#
+# Copyright (c) 2025-now Institut des Géosciences de l'Environnement (UMR 5001).
+#
+# License: BSD 3-Clause "New" or "Revised" License (BSD-3-Clause).
+#
+# This script installs the HDF5 library.
+#
+# This script should be run from its directory.
+#
+
+set -e
+
+source ../parameters.conf
+source parameters.conf
+
+while [[ $# -gt 0 ]]; do
+    case $1 in
+        -d|--destination)
+            dir_dest=$2
+            shift
+            shift
+            ;;
+        *)
+            echo "Error: unknown command-line argument: $1"
+            exit 1
+            ;;
+    esac
+done
+
+if [[ -z ${dir_dest} ]]; then
+    echo "Error: please specify destination directory"
+    exit 1
+fi
+
+dir_work=./work_netcdf-c
+if [[ -d ${dir_work} || -f ${dir_work} ]]; then
+    echo "Error: work directory already exists (as file or directory)"
+    exit 1
+fi
+
+echo "Cloning from repository: ${url_repo}"
+git clone ${url_repo} ${dir_work}
